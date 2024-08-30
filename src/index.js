dba04ef8530f3ec1fd14f2fd663b756edab2d174
Pravin <pravindaware9221@gmail.com>
2024-08-30 12:36:37 +0530

    create react app

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import {createRoot} from "react-dom/client"
+const root = createRoot(document.getElementById("root"));
+root.render(<h1>React app is successefully work
+    <h2>Hii! Pravin </h2></h1>);
